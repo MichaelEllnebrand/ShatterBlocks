870e1fea51d8e5017279e07dfc4d52bbb528a892
Michael Ellnebrand <denatus@denatus.com>
2022-03-12 23:53:29 +0100

    Added column indicator and block spawner

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -5,37 +5,75 @@ using UnityEngine;
 public class BlockManager : MonoBehaviour
 {
     [SerializeField] GameObject pfBlock;
+    [SerializeField] GameObject ghost;
     [SerializeField] Vector3 position;
     [SerializeField] int Width;
     [SerializeField] int Height;
 
     private bool[,] isOccupied;
-
     
+    [SerializeField] private float spawnTimerMax;
+    private float spawnTimer;
+    private int ghostColumn = 7;
 
-
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         transform.position = position;
         isOccupied = new bool[Width, Height];
-
-        
-        
+        spawnTimer = spawnTimerMax;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f)
         {
-            SpawnBlock();
+            spawnTimer += spawnTimerMax;
+            if (!SpawnBlockAtGhost())
+            {
+                Debug.Log("GAME OVER");
+            }
         }
+        
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ghostColumn--;
+            if (ghostColumn < 0) ghostColumn = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ghostColumn++;
+            if (ghostColumn > Width-1) ghostColumn = Width-1;
+        }
+        SetGhostPosition();
     }
 
+    void SetGhostPosition()
+    {
+        ghost.transform.position = new Vector3(position.x + ghostColumn * ghost.transform.localScale.x, ghost.transform.position.y, ghost.transform.position.z);
+    }
 
+    private bool SpawnBlockAtGhost()
+    {
+        bool isSpawned = false;
+        int x = ghostColumn;
+        for (int y = 0; y < Height; y++)
+        {
+            if (!isOccupied[x, y] && !isSpawned)
+            {
+                isSpawned = true;
+                isOccupied[ghostColumn, y] = true;
+                Vector3 pos = new Vector3(position.x + x * pfBlock.transform.localScale.x, position.y + y * pfBlock.transform.localScale.y, 0);
+                GameObject b = Instantiate(pfBlock, pos, Quaternion.identity, transform);
+                b.transform.position = pos;
+                b.GetComponent<Block>().SetPostion(x, y);
+            }
+        }
+        return isSpawned;
+    }
 
-    void SpawnBlock()
+    void SpawnBlockRandom()
     {
         int x = Random.Range(0,Width);
         int y = Random.Range(0,Height);
@@ -56,6 +94,5 @@ public class BlockManager : MonoBehaviour
         {
             isOccupied[x, y] = false;
         }
-
     }
 }
